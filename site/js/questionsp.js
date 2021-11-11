38c6bc8e45f251407de29b27ff1d0243af4a195e
Elsaa57 <93739895+Elsaa57@users.noreply.github.com>
2021-11-11 20:47:53 +0100

    Add files via upload

---
+++ site/js/questionsp.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Comment dit-on 'où' en anglais ?",
+    answer: "Where",
+    options: [
+      "Where",
+      "How",
+      "What",
+      "Whose"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Comment dit-on 'quand' en anglais ?",
+    answer: "When",
+    options: [
+      "How long",
+      "Why",
+      "When",
+      "Who"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Comment dit-on 'pourquoi' en anglais ?",
+    answer: "Why",
+    options: [
+      "How long",
+      "What",
+      "Whose",
+      "Why"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Comment dit-on 'comment' en anglais ?",
+    answer: "How",
+    options: [
+      "When",
+      "Where",
+      "How",
+      "Who"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Comment dit-on 'qui' en anglais ?",
+    answer: "Who",
+    options: [
+      "How old",
+      "Who",
+      "Where",
+      "How long"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Comment dit-on 'à qui' en anglais ?",
+    answer: "Whose",
+    options: [
+      "Whose",
+      "How",
+      "How long",
+      "When"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Comment dit-on 'que, quel' en anglais ?",
+    answer: "What",
+    options: [
+      "Who",
+      "Why",
+      "Whose",
+      "What"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Comment dit-on 'lequel' en anglais ?",
+    answer: "Which",
+    options: [
+      "How old",
+      "Which",
+      "Whose",
+      "Where"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Pour parler de l'âge de quelqu'un on dit ?",
+    answer: "How old",
+    options: [
+      "How old",
+      "What",
+      "Which",
+      "Why"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Pour parler d'une durée on dit ?",
+    answer: "How long",
+    options: [
+      "How",
+      "How old",
+      "Whose",
+      "How long"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
